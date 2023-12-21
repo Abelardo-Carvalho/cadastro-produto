b2b413b5c2916caf1d3fdeea320f6ac052cf896b
Abelardo-Carvalho <abelardocarvalhoneto@gmail.com>
2023-12-20 22:53:46 -0300

    first commit

---
+++ src/main/java/com/example/springboot/models/ProductModel.java
@@ -0,0 +1,12 @@
+package com.example.springboot.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name = "TB-PRODCTS")
+public class ProductModel  implements Serializable {
+    private static final long serialVersionUID = 1L;
+}
